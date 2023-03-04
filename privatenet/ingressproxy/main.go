09eeb7c8806f07763edf293f035f76d4e20b16a1
Five <five@railway.app>
2023-03-04 16:26:14 +0800

    privatenet/ingressproxy: netstack.CreateNetTUN()

--- privatenet/ingressproxy/main.go
+++ privatenet/ingressproxy/main.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"log"
+	"net/netip"
+
+	"golang.zx2c4.com/wireguard/conn"
+	"golang.zx2c4.com/wireguard/device"
+	"golang.zx2c4.com/wireguard/tun/netstack"
+)
+
+func main() {
+	tun, tnet, err := netstack.CreateNetTUN(
+		[]netip.Addr{netip.MustParseAddr("192.168.4.29")},
+		[]netip.Addr{},
+		device.DefaultMTU,
+	)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	dev := device.NewDevice(tun, conn.NewDefaultBind(), device.NewLogger(device.LogLevelVerbose, ""))
+	dev.Up()
+}
