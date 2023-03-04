8df09a8fcd1764ec60fa4a265526ec52ae3c39f1
Five <five@railway.app>
2023-03-04 13:54:40 +0800

    privatenet/testserver: http.ListenAndServe()

--- privatenet/testserver/main.go
+++ privatenet/testserver/main.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+)
+
+var (
+	port string
+)
+
+func init() {
+	flag.StringVar(&port, "port", "80", "")
+}
+
+func main() {
+	flag.Parse()
+
+	mux := http.NewServeMux()
+	mux.Handle("/api", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {}))
+	mux.Handle("/sin", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {}))
+	mux.Handle("/iad", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {}))
+	mux.Handle("/cdg", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {}))
+	log.Fatal(http.ListenAndServe(":"+port, mux))
+}
