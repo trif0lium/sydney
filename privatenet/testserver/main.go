985f775134370efed26fb7b12cf8168280d64405
Five <five@railway.app>
2023-03-04 14:13:30 +0800

    privatenet/testserver: implement `/api` handler

--- privatenet/testserver/main.go
+++ privatenet/testserver/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 var (
@@ -18,7 +20,29 @@ func main() {
 	flag.Parse()
 
 	mux := http.NewServeMux()
-	mux.Handle("/api", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {}))
+	mux.Handle("/api", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		hostname, _ := os.Hostname()
+		data := struct {
+			Hostname string      `json:"hostname,omitempty"`
+			IP       []string    `json:"ip,omitempty"`
+			Headers  http.Header `json:"header,omitempty"`
+			URL      string      `json:"url,omitempty"`
+			Host     string      `json:"host,omitempty"`
+			Method   string      `json:"method,omitempty"`
+		}{
+			Hostname: hostname,
+			IP:       []string{},
+			Headers:  r.Header,
+			URL:      r.URL.RequestURI(),
+			Host:     r.Host,
+			Method:   r.Method,
+		}
+		w.Header().Set("Content-Type", "application/json")
+		if err := json.NewEncoder(w).Encode(data); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}))
 	mux.Handle("/sin", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {}))
 	mux.Handle("/iad", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {}))
 	mux.Handle("/cdg", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {}))
