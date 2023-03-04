20bd1f15c23f08d06627766203c3d6be473d27d4
Five <five@railway.app>
2023-03-04 14:58:45 +0800

    privatenet/testserver: http.Get()

--- privatenet/testserver/main.go
+++ privatenet/testserver/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"encoding/json"
 	"flag"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -20,8 +22,10 @@ func main() {
 	flag.Parse()
 
 	mux := http.NewServeMux()
+
 	mux.Handle("/api", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		hostname, _ := os.Hostname()
+
 		data := struct {
 			Hostname string      `json:"hostname,omitempty"`
 			IP       []string    `json:"ip,omitempty"`
@@ -37,14 +41,76 @@ func main() {
 			Host:     r.Host,
 			Method:   r.Method,
 		}
+
 		w.Header().Set("Content-Type", "application/json")
 		if err := json.NewEncoder(w).Encode(data); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 	}))
-	mux.Handle("/sin", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {}))
-	mux.Handle("/iad", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {}))
-	mux.Handle("/cdg", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {}))
+
+	mux.Handle("/sin", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		resp, err := http.Get("http://testserver.sin.internal/api")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		_, _ = fmt.Fprintln(w, string(body))
+		if err := r.Write(w); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}))
+
+	mux.Handle("/iad", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		resp, err := http.Get("http://testserver.iad.internal/api")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		_, _ = fmt.Fprintln(w, string(body))
+		if err := r.Write(w); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}))
+
+	mux.Handle("/cdg", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		resp, err := http.Get("http://testserver.cdg.internal/api")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		_, _ = fmt.Fprintln(w, string(body))
+		if err := r.Write(w); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}))
+
 	log.Fatal(http.ListenAndServe(":"+port, mux))
 }
