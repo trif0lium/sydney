2f371f9b59b79b36fd16fb85921c0fefe891e111
Five <five@railway.app>
2023-03-05 10:26:31 +0800

    privatenet/egressproxy: dev.Up()

--- privatenet/egressproxy/main.go
+++ privatenet/egressproxy/main.go
@@ -1,1 +1,56 @@
 package main
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"log"
+	"net/netip"
+	"os"
+	"path/filepath"
+
+	"golang.zx2c4.com/wireguard/conn"
+	"golang.zx2c4.com/wireguard/device"
+	"golang.zx2c4.com/wireguard/tun/netstack"
+)
+
+func main() {
+	tun, tnet, err := netstack.CreateNetTUN(
+		[]netip.Addr{netip.MustParseAddr("192.168.4.28")},
+		[]netip.Addr{},
+		device.DefaultMTU,
+	)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	dev := device.NewDevice(tun, conn.NewDefaultBind(), device.NewLogger(device.LogLevelVerbose, ""))
+
+	cwd, err := os.Getwd()
+	if err != nil {
+		log.Panic(err)
+	}
+
+	localPrivateKey, err := os.ReadFile(filepath.Join(cwd, ".wg/iad"))
+	if err != nil {
+		log.Panic(err)
+	}
+
+	removePublicKey, err := os.ReadFile(filepath.Join(cwd, ".wg/sin.pub"))
+	if err != nil {
+		log.Panic(err)
+	}
+
+	wgConf := bytes.NewBuffer(nil)
+	fmt.Fprintf(wgConf, "private_key=%s\n", string(localPrivateKey))
+	fmt.Fprintf(wgConf, "public_key=%s\n", string(removePublicKey))
+	fmt.Fprintf(wgConf, "endpoint=%s\n", "host.docker.internal:58120")
+	fmt.Fprintf(wgConf, "allowed_ip=%s\n", "192.168.4.28/32")
+	fmt.Fprintf(wgConf, "persistent_keepalive_interval=25\n")
+
+	if err := dev.IpcSetOperation(bufio.NewReader(wgConf)); err != nil {
+		log.Panic(err)
+	}
+
+	dev.Up()
+}
