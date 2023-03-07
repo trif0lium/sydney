c8805775337d1dfd24e8f64cc506ce35360218b0
Five <five@railway.app>
2023-03-07 13:39:09 +0800

    cloudhv: remove `--min-cpu-platform`

--- cloudhv/dev-vm.sh
+++ cloudhv/dev-vm.sh
@@ -1,5 +1,4 @@
 gcloud compute instances create cloudhv-dev-vm \
   --enable-nested-virtualization \
   --zone asia-southeast1-a \
-  --min-cpu-platform "Intel Haswell" \
   --machine-type "n2-standard-4"
