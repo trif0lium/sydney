b7e024122b5c8b7596954e9e0e0fa56e22f6b39f
Five <five@railway.app>
2023-03-10 12:20:14 +0800

    cloudhv: ubuntu-2210

--- cloudhv/dev-vm.sh
+++ cloudhv/dev-vm.sh
@@ -2,4 +2,5 @@ gcloud compute instances create cloudhv-dev-vm \
   --enable-nested-virtualization \
   --zone australia-southeast2-a \
   --machine-type "n2-standard-8" \
-  --boot-disk-size "100GB"
+  --boot-disk-size "100GB" \
+  --image "projects/ubuntu-os-cloud/global/images/ubuntu-2210-kinetic-amd64-v20230125"
