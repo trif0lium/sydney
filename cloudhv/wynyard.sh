3ace4387e67c0dc6a195e591693f808113efedec
Five <five@railway.app>
2023-03-14 11:22:23 +0800

    cloudhv/wynyard: --image-project

--- cloudhv/wynyard.sh
+++ cloudhv/wynyard.sh
@@ -3,4 +3,5 @@ gcloud compute instances create wynyard-0 \
   --zone australia-southeast2-a \
   --machine-type "n1-standard-32" \
   --boot-disk-size "100GB" \
-  --image-family "debian-11"
+  --image-family "debian-11" \
+  --image-project "debian-cloud"
