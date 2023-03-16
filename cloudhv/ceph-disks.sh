ff58e646bda6fc04fd10de587e21ef73735afab0
Five <five@railway.app>
2023-03-16 10:41:41 +0800

    cloudhv/ceph-disks: \

--- cloudhv/ceph-disks.sh
+++ cloudhv/ceph-disks.sh
@@ -4,11 +4,11 @@ gcloud compute disks create ceph-0-disk-0 \
   --zone australia-southeast2-a
 gcloud compute disks create ceph-0-disk-1 \
   --size 50 \
-  --type "pd-ssd"
+  --type "pd-ssd" \
   --zone australia-southeast2-a
 gcloud compute disks create ceph-0-disk-2 \
   --size 50 \
-  --type "pd-ssd"
+  --type "pd-ssd" \
   --zone australia-southeast2-a
 gcloud compute instances attach-disk ceph-0 --disk ceph-0-disk-0
 gcloud compute instances attach-disk ceph-0 --disk ceph-0-disk-1
