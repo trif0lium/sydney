971613029ee55a35b79b94d5220ab4a26c201ae2
Five <five@railway.app>
2023-03-16 10:11:56 +0800

    cloudhv: init ceph.sh

---
+++ cloudhv/ceph.sh
@@ -0,0 +1,7 @@
+gcloud compute instances create wynyard-0 \
+  --enable-nested-virtualization \
+  --zone australia-southeast2-a \
+  --machine-type "n1-standard-32" \
+  --boot-disk-size "100GB" \
+  --image-family "debian-11" \
+  --image-project "debian-cloud"
