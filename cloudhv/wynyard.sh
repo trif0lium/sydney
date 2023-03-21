1d21217cf8ac24222b04900dac33f8e8ca09c89c
Five <five@railway.app>
2023-03-21 12:09:05 +0800

    wynyard: wynyard-0-disk-0

--- cloudhv/wynyard.sh
+++ cloudhv/wynyard.sh
@@ -5,3 +5,8 @@ gcloud compute instances create wynyard-0 \
   --boot-disk-size "100GB" \
   --image-family "debian-11" \
   --image-project "debian-cloud"
+gcloud compute disks create wynyard-0-disk-0 \
+  --size 50 \
+  --type "pd-ssd" \
+  --zone australia-southeast2-a
+gcloud compute instances attach-disk wynyard-0 --disk wynyard-0-disk-0
